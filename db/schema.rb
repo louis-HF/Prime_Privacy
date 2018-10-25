7f9bf82b47d214d2d1d57b26ac89b37b3ba8692e
louis HERR FENDER <louis.hf.67@gmail.com>
2018-10-25 18:49:08 +0200

    added buttons and social network components

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_121214) do
+ActiveRecord::Schema.define(version: 2018_10_25_160215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2018_10_25_121214) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.string "facebook_token"
+    t.string "twitter_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
