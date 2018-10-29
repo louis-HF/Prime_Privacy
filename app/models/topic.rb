c3ba16fc09f1d80364f83d13af99ec9c988d8eea
louis HERR FENDER <louis.hf.67@gmail.com>
2018-10-29 18:06:19 +0100

    bug fixed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ApplicationRecord
   belongs_to :user
   has_many :preferences, dependent: :destroy
   has_many :keywords, dependent: :destroy
-  validates: :name, presence: true
+  validates :name, presence: true
 end
