2952b0033c8f44d816247fe190884ac8f7ca4919
louis HERR FENDER <louis.hf.67@gmail.com>
2018-10-31 18:03:18 +0100

    2nd dashboard finished apart from action button

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@
 user = User.last
 user.facebook_token = 3
 
-Topic.create(name: "test")
+Userstatistic.create(user:user, date:"2018-02-02", fb_photo_public:4, fb_photo_public_sensitive:1, fb_post_public:5, fb_post_public_sensitive:2, fb_likes_public:2, fb_likes_public_sensitive:1, tw_photo_public:15, tw_photo_public_sensitive:3, tw_post_public:0, tw_post_public_sensitive:0, tw_likes_public:0, tw_likes_public_sensitive:0, total_fb_public:10, total_fb_public_sensitive:3, total_tw_public:12, total_tw_public_sensitive:4)
+Userstatistic.create(user:user, date:"2018-04-04", fb_photo_public:4, fb_photo_public_sensitive:1, fb_post_public:5, fb_post_public_sensitive:2, fb_likes_public:2, fb_likes_public_sensitive:1, tw_photo_public:15, tw_photo_public_sensitive:3, tw_post_public:0, tw_post_public_sensitive:0, tw_likes_public:0, tw_likes_public_sensitive:0, total_fb_public:8, total_fb_public_sensitive:1, total_tw_public:9, total_tw_public_sensitive:2)
+Userstatistic.create(user:user, date:"2018-09-09", fb_photo_public:4, fb_photo_public_sensitive:1, fb_post_public:5, fb_post_public_sensitive:2, fb_likes_public:2, fb_likes_public_sensitive:1, tw_photo_public:15, tw_photo_public_sensitive:3, tw_post_public:0, tw_post_public_sensitive:0, tw_likes_public:0, tw_likes_public_sensitive:0, total_fb_public:7, total_fb_public_sensitive:0, total_tw_public:10, total_tw_public_sensitive:3)
 #long-term seeds !!!! DON'T DELE
 #DONT DELETE
 #DONT DELETE
