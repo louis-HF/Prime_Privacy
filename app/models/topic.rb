eea1b8a69132308d3c6ca9d3f2d240135c21b82a
louis HERR FENDER <louis.hf.67@gmail.com>
2018-10-31 15:48:29 +0100

    advanced data dashboard

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :preferences, dependent: :destroy
   has_many :keywords, dependent: :destroy
   has_many :topicstatistics
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: {scope: :user}
 end
