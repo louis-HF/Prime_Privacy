be4fa39754b5f6a94793f248429a375c0fa73ea5
louis HERR FENDER <louis.hf.67@gmail.com>
2018-11-02 12:16:04 +0100

    test

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,9 @@
 class AuthenticationsController < ApplicationController
   def index
    @authentications = policy_scope(Authentications) if current_user
-  end
+ end
 
-  def create
+ def create
    omniauth = request.env["omniauth.auth"]
    authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
    if authentication
@@ -31,7 +31,4 @@ class AuthenticationsController < ApplicationController
    @authentication.destroy
    flash[:notice] = "Successfully destroyed authentication."
    redirect_to authentications_url
-  end
 end
-
-
