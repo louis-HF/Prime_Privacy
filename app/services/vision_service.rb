5d8d5ed201852ff927c2953ee1365951925ac7cc
Mathieu Wendi <mathieu.wendi@edhec.com>
2018-11-06 15:56:56 +0100

    vision api working

--- app/services/vision_service.rb
+++ app/services/vision_service.rb
@@ -2,31 +2,40 @@ require 'json'
 require 'open-uri'
 require 'rest-client'
 require "google/cloud/vision"
+require 'json'
+
+class VisionService
+  def initialize(photo_url)
+    @photo_url = photo_url
+    # @key = ENV['GOOGLE_VISION_API_KEY']
+    @key = 'AIzaSyCY7pP59zKlhQZFoHMi584dLIw2VRT_is8'
+  end
+
+  def call
+    url = 'https://vision.googleapis.com/v1/images:annotate?key=' + @key
+    payload = {
+      requests: [
+        {
+          image: {
+            source: {
+              imageUri: @photo_url
+            }
+          },
+          features: [
+            {
+              type: "OBJECT_LOCALIZATION"
+              # maxResults: 15
+            }
+          ]
+        }
+      ]
+    }
+    JSON.parse(RestClient.post(url, payload.to_json, content_type: 'application/json'))["responses"][0]
+  end
+end
 
 
-
-# vision = Google::Cloud::Vision.new project: project_id
-
-# image = vision.image "https://images.unsplash.com/photo-1532635241-17e820acc59f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3eb43b897f17dfd077c53caaef4c14ed&auto=format&fit=crop&w=1003&q=80"
-# annotation = vision.annotate image, labels: true
-# puts annotation.labels.count
-# puts annotation.labels
-
-#  def detect_labels
-  # [START vision_label_detection]
-  # project_id = "Your Google Cloud project ID"
-  # image_path = "Path to local image file, eg. './image.png'"
-#  project_id = "primeprivacy-221608"
-#  image_path = "https://images.unsplash.com/photo-1532635241-17e820acc59f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3eb43b897f17dfd077c53caaef4c14ed&auto=format&fit=crop&w=1003&q=80"
-
-  #  require "google/cloud/vision"
-
-  #vision = Google::Cloud::Vision.new project: project_id
-  #image  = vision.image image_path
-
-  #image.labels.each do |label|
-   # puts label.description
- # end
-#end
-
-#detect_labels
+# response = VisionService.new("https://static.vinepair.com/wp-content/uploads/2017/07/beer-dogs-inside.jpg").call
+# response[0]["localizedObjectAnnotations"].each do |e|
+#   puts e['name']
+# end
