11386d7c93600a6b81ac97d26f022de07dc2e2d1
louis HERR FENDER <louis.hf.67@gmail.com>
2018-11-07 18:37:25 +0100

    before merging

--- app/services/facebook_service.rb
+++ app/services/facebook_service.rb
@@ -94,7 +94,7 @@ class FacebookService
   def facebook_images_feed(content)
     if !content["full_picture"].include?("photos") && !content["full_picture"].include?("external.xx")
 
-      cloudinaryurl = Cloudinary::Uploader.upload(content["full_picture"])["url"]
+      cloudinaryurl = Cloudinary::Uploader.upload(content["full_picture"])["url"])
       Content.create(
               user: @current_user,
               external_provider: "facebook",
