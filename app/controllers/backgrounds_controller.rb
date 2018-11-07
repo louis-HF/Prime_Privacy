962128f6b11180233a3ce8178b024c77322eddb2
louis HERR FENDER <louis.hf.67@gmail.com>
2018-11-07 16:02:14 +0100

    fixed

--- app/controllers/backgrounds_controller.rb
+++ app/controllers/backgrounds_controller.rb
@@ -1,9 +1,12 @@
 class BackgroundsController < ApplicationController
   skip_after_action :verify_authorized, only: :loadingpage
+
   def loadingpage
+    render 'backgrounds/loadingpage',layout: 'application'
     PreferenceInitializer.new(current_user).create_pref if Preference.where(user: current_user).empty?
     if Content.where(user: current_user, scan_date: Date.today).empty?
       FacebookJob.perform_later(current_user.id)
     end
   end
+
 end
